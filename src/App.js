3897d7041e241d6b25324322725af3f53691eec3
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-03-18 07:11:29 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import HomePage from "./features/home/HomePage"
-import Footer from "./ui/Footer"
-import Header from "./ui/Header"
-
-
-
 const App = () => {
 
 
   return (
     <div>
-
-      <Header />
-      <div className=" border-b-2 border-black "></div>
-      <HomePage />
-      <div className=" border-b-2 border-black "></div>
-      <Footer />
 
 
 
