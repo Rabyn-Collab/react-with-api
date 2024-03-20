55fb3ac4495c6e17ee22e30e2bdf7046da2cf4dd
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-03-20 07:12:44 +0545

    Update Detail.js
    kjhkjhjk

--- src/features/home/Detail.js
+++ src/features/home/Detail.js
@@ -2,15 +2,14 @@ import { useParams } from "react-router"
 
 const Detail = () => {
 
-  const ni = useParams();
+ 
 
   console.log(ni);
 
   return (
     <div>
 
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur vel magnam error modi veritatis quisquam nulla repellat fugit repellendus totam deleniti optio officiis soluta hic nisi explicabo quidem asperiores commodi tempora suscipit rem, tempore obcaecati. Dolorum a odit quam similique nostrum ipsa ipsum exercitationem doloremque repudiandae quo modi ullam, temporibus, dolore illum molestias? Autem labore aperiam ducimus sunt. Quod reiciendis, placeat mollitia aut delectus laborum molestias, architecto veritatis, eum nihil expedita dolor! Sapiente amet maiores recusandae expedita nam dignissimos placeat repellat nesciunt, sequi architecto nostrum itaque eaque culpa a dolorem cupiditate tenetur eius harum. Necessitatibus animi corporis molestias aliquid, earum minus nesciunt facilis, veniam blanditiis, soluta doloribus fugiat nisi deserunt maxime temporibus tempore laudantium exercitationem obcaecati quo ipsam facere dolore voluptatem? Omnis magnam, eveniet labore voluptas nobis doloribus voluptatem, praesentium eum aut dignissimos iste ipsum excepturi laboriosam? Adipisci corrupti, officia illo eligendi qui consectetur ipsam assumenda tempora quaerat facilis suscipit odit sequi nam velit rem, totam aperiam tempore perspiciatis beatae explicabo voluptatibus obcaecati doloribus! Animi incidunt beatae veniam voluptatum eos earum fugiat veritatis quas sint temporibus voluptates consequatur laboriosam cupiditate excepturi tempora, quia ut quidem voluptate odit, sit adipisci sapiente necessitatibus? Officiis doloribus labore veniam blanditiis nemo distinctio accusantium sint aspernatur quasi ex earum illum, aliquam dignissimos fugiat provident inventore suscipit quod eos corrupti accusamus praesentium? Assumenda placeat praesentium quos debitis, officia nostrum pariatur rem perferendis delectus, maxime earum perspiciatis quas vel ipsa at quam. Unde consectetur alias voluptatibus quaerat hic sunt id porro saepe ullam animi debitis et voluptatum distinctio iste repellat, aliquid fugiat. Ab sed quas dicta nemo ipsam! Libero iure aspernatur quia, nobis obcaecati cupiditate doloribus quo, ratione omnis sint, assumenda nemo suscipit? Aliquam cupiditate iste beatae nisi fuga minima ipsa quasi vitae accusantium! Delectus, praesentium consequuntur. Ad unde nisi repellat suscipit explicabo omnis incidunt reiciendis totam?</p>
-
+    
 
 
     </div>
