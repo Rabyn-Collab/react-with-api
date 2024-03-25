430f32a62387643ebcb9c6f093b20f629ce17399
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-03-25 08:01:40 +0545

    ch

--- src/features/Home.js
+++ src/features/Home.js
@@ -1,6 +1,28 @@
+import { useState } from "react";
+
+
 const Home = () => {
+
+  const [text, setText] = useState('first value');
+
+
+
+
+  const handleChange = (msg) => {
+    setText((prev) => {
+      console.log(prev);
+      return msg;
+    });
+  }
+
+  console.log('render');
+
   return (
-    <div>
+    <div className="p-2">
+
+      <button onClick={() => handleChange('lio')} className="px-2  border-4 border-black   bg-black text-white font-serif font-boldrounded-lg">Click me</button>
+
+      <h1>hello text----- {text}</h1>
 
 
     </div>
