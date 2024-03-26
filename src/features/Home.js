06e2649707b2d3915f14013782c8b1746af75e27
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-03-26 07:57:51 +0545

    changes

--- src/features/Home.js
+++ src/features/Home.js
@@ -1,12 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const Home = () => {
 
   const [text, setText] = useState('first value');
-
-
-
+  const [depend, setDepend] = useState(0);
 
   const handleChange = (msg) => {
     setText((prev) => {
@@ -15,10 +13,27 @@ const Home = () => {
     });
   }
 
+  const handleDepend = () => {
+    setDepend((prev) => prev + 1);
+  }
+
+
+  useEffect(() => {
+    console.log('effect call');
+    setText('sd;lsd;flkk;sfdl');
+  }, [depend]);
+
+
   console.log('render');
 
   return (
     <div className="p-2">
+
+      <h1>{depend}</h1>
+
+      <button onClick={handleDepend} className="px-2  border-4 border-black   bg-black text-white font-serif font-boldrounded-lg">InCrement</button>
+      <br />
+      <br />
 
       <button onClick={() => handleChange('lio')} className="px-2  border-4 border-black   bg-black text-white font-serif font-boldrounded-lg">Click me</button>
 
--- src/ui/Header.js
+++ src/ui/Header.js
@@ -1,12 +1,35 @@
 import { NavLink } from "react-router-dom"
-
+import { HiMenuAlt1, HiOutlineX } from "react-icons/hi";
+import { useState } from "react";
 const Header = () => {
+
+  const [show, setShow] = useState(false);
+  const handleShow = () => {
+    setShow((prev) => !prev);
+  }
+
+
+
+
+
 
 
   return (
-    <div className="bg-black text-white px-5 flex items-baseline justify-between py-2 sticky top-0 z-10">
-      <h1>Web Logo</h1>
-      <nav className="space-x-4">
+    <div className="bg-black text-white px-5 flex sm:items-start items-center justify-between  py-2 sticky top-0 z-10">
+      <div>
+        <h1 className="mb-2">Web Logo</h1>
+
+        {show && <nav className="sm:flex sm:flex-col hidden space-y-2">
+          <NavLink to='/about'> About</NavLink>
+          <NavLink to='/contact'>Contact</NavLink>
+        </nav>}
+
+      </div>
+
+      <div className="mt-1 hidden sm:flex">
+        {show ? <button onClick={handleShow}><HiOutlineX size={25} /></button> : <button onClick={handleShow}><HiMenuAlt1 size={25} /></button>}
+      </div>
+      <nav className="space-x-4 sm:hidden">
         <NavLink to='/about'> About</NavLink>
         <NavLink to='/contact'>Contact</NavLink>
       </nav>
